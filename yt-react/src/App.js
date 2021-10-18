fd41fd5e977e7876ee7b0c18724b1ab3fb32e28b
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-10-18 02:17:49 -0300

    add prop and state

--- yt-react/src/App.js
+++ yt-react/src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+class Mybutton extends Component{
+  render(){
+    return (
+    <button onClick={() => {this.props.handleClick(this.props.label);}}>
+      {this.props.label}
+      
+       </button>
+    );
+  }
+}
+
+class MyLabel extends Component{
+  render(){
+    return <p>Click no: {this.props.text}</p>
+  }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      labelText: '',
+    };
+  }
+
+  setLabelText = (labelText) => {
+    this.setState({labelText});
+  }
+
+  render(){
+  console.log(this.stage)
   return (
-    <p>Hellow World</p>
+    <div>
+    <MyLabel text={this.state.labelText}/>
+    <Mybutton handleClick={this.setLabelText} label="Botão 1"/>
+    <Mybutton handleClick={this.setLabelText} label="Botão 2"/>
+    <Mybutton handleClick={this.setLabelText} label="Botão 3"/>
+    
+
+    </div>
   );
+  }
 }
 
 export default App;
